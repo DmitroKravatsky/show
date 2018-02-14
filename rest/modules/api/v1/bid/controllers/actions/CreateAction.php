d723d46c0dd9d765a2b769d026ef258d9866c24d
timur <ablyalimovtim@gmail.com>
2018-02-14 18:45:47 +0300

    Fix

--- rest/modules/api/v1/bid/controllers/actions/CreateAction.php
+++ rest/modules/api/v1/bid/controllers/actions/CreateAction.php
@@ -2,53 +2,16 @@
 
 namespace rest\modules\api\v1\bid\controllers\actions;
 
-use common\behaviors\ValidatePostParameters;
 use common\models\bid\BidEntity;
-use rest\modules\api\v1\bid\controllers\BidController;
 use yii\web\ServerErrorHttpException;
 use Yii;
 
 /**
  * Class CreateAction
- * @mixin ValidatePostParameters
  * @package rest\modules\api\v1\comment\controllers\actions
  */
 class CreateAction extends \yii\rest\Action
 {
-    /** @var  BidController */
-    public $controller;
-
-    /**
-     * @var array
-     */
-    public $params = [];
-
-    /**
-     * @return array
-     */
-    public function behaviors(): array
-    {
-        return [
-            'reportParams' => [
-                'class'       => ValidatePostParameters::className(),
-                'inputParams' => [
-                    'from_wallet', 'to_wallet', 'from_currency', 'to_currency', 'name', 'last_name', 'email',
-                    'phone_number', 'from_sum', 'to_sum','from_payment_system', 'to_payment_system'
-                ]
-            ],
-        ];
-    }
-
-    /**
-     * @return bool
-     * @throws \yii\web\BadRequestHttpException
-     */
-    protected function beforeRun(): bool
-    {
-        $this->validationParams();
-        return parent::beforeRun();
-    }
-
     /**
      * @return mixed
      * @throws ServerErrorHttpException
--- rest/modules/api/v1/bid/controllers/actions/UpdateAction.php
+++ rest/modules/api/v1/bid/controllers/actions/UpdateAction.php
@@ -2,49 +2,16 @@
 
 namespace rest\modules\api\v1\bid\controllers\actions;
 
-use common\behaviors\ValidatePostParameters;
 use common\models\bid\BidEntity;
 use Yii;
 use yii\web\ServerErrorHttpException;
 
 /**
  * Class UpdateAction
- * @mixin ValidatePostParameters
  * @package rest\modules\api\v1\bid\controllers\actions
  */
 class UpdateAction extends \yii\rest\Action
 {
-    /**
-     * @var array
-     */
-    public $params = [];
-
-    /**
-     * @return array
-     */
-    public function behaviors(): array
-    {
-        return [
-            'reportParams' => [
-                'class'       => ValidatePostParameters::className(),
-                'inputParams' => [
-                    'from_wallet', 'to_wallet', 'from_currency', 'to_currency', 'name', 'last_name', 'email',
-                    'phone_number', 'from_sum', 'to_sum', 'from_payment_system', 'to_payment_system',
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * @return bool
-     * @throws \yii\web\BadRequestHttpException
-     */
-    protected function beforeRun(): bool
-    {
-        $this->validationParams();
-        return parent::beforeRun();
-    }
-
     /**
      * @param $id
      * @return array
@@ -54,9 +21,7 @@ class UpdateAction extends \yii\rest\Action
      */
     public function run($id)
     {
-        /** @var BidEntity $bid */
         $bid = new BidEntity();
-        
         return $bid->updateBid($id);
     }
 }
