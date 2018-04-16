9441401124479b61878fe1b8d2ba161c536954ac
Dima <kravatsky.dmitrij@yandex.ua>
2018-04-16 17:46:44 +0300

    change logic in gmail auth

---
+++ rest/modules/api/v1/authorization/controllers/actions/social/GmailAuthorizationAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 16.04.18
+ * Time: 15:46
+ */
+
+namespace rest\modules\api\v1\authorization\controllers\actions\social;
+
+
+class GmailAuthorizationAction
+{
+
+}
