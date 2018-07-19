cd8f71b0025ca7c533bd755e36069733ebd3f7f0
Dima <kravatsky.dmitrij@yandex.ua>
2018-07-19 10:40:26 +0300

    new login form

--- backend/modules/authorization/views/authorization/login.php
+++ backend/modules/authorization/views/authorization/login.php
@@ -16,41 +16,19 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="alert alert-danger" role="alert"><?php echo Yii::$app->session->getFlash('Enter_failed')?></div>
     <?php endif;?>
 
-<div class="limiter">
-    <div class="container-login100">
-        <div class="wrap-login100 p-t-190 p-b-30">
-            <?php $form = ActiveForm::begin(['id' => 'login-form',
-                'options' => ['class' => 'login100-form validate-form']
+<div class="card card-container">
+        <div style="text-align: center" ><h2>Admin panel</h2></div>
+        <?php $form = ActiveForm::begin(['id' => 'login-form',
+                'options' => ['class' => 'form-signin']
             ]); ?>
-
-                <div class="wrap-input100 validate-input m-b-10" >
-                    <?= $form->field($modelLogin, 'phone_number')->textInput([
-                            'autofocus' => true , 'class' => 'input100', 'placeholder' => "Enter Your Phone Number"
+        <span id="reauth-email" class="reauth-email"></span>
+        <?= $form->field($modelLogin, 'phone_number')->textInput([
+                            'autofocus' => true , 'class' => 'form-control', 'id'=>"inputEmail",  'placeholder' => "Enter Your Phone Number"
                     ])->label(false) ?>
-                    <span class="symbol-input100">
-							<i class="fa fa-user"></i>
-                    </span>
-                </div>
-
-                <div class="wrap-input100 validate-input m-b-10">
-                    <?= $form->field($modelLogin, 'password')->passwordInput([
-                            'class' => 'input100', 'placeholder' => "Enter Your Password"
-                    ])->label(false) ?>
-                    <span class="symbol-input100">
-							<i class="fa fa-lock"></i>
-                    </span>
-                </div>
-                <div class="wrap-input100 validate-input m-b-10">
-                    <?= $form->field($modelLogin, 'rememberMe')->checkbox()->label("remember me") ?>
-                    <span class="symbol-input100">
-                                <i class="fa fa-lock"></i>
-                    </span>
-                </div>
-                <div class="container-login100-form-btn p-t-10">
-                    <?= Html::submitButton('Login', ['class' => 'login100-form-btn', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-
-        </div>
-    </div>
-</div>
+        <?= $form->field($modelLogin, 'password')->passwordInput([
+                            'class' => 'form-control', 'id'=>"inputPassword", 'placeholder' => "Enter Your Password"
+                    ])->label(false) ?><div id="remember" class="checkbox">
+        <?= $form->field($modelLogin, 'rememberMe')->checkbox(['class' => "checkbox" ])->label("Remember me") ?>
+        <?= Html::submitButton('Login', ['class' => 'btn btn-lg btn-primary btn-block btn-signin', 'name' => 'login-button']) ?>
+        <?php ActiveForm::end(); ?>
+</div><!-- /card-container -->
