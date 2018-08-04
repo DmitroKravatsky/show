d5acbc0949e3c7e9a70ab7b3ea8be59f0d3897c2
Dima <kravatsky.dmitrij@yandex.ua>
2018-08-04 07:35:29 +0300

    use panel widjet

--- backend/modules/admin/views/admin/invite-manager.php
+++ backend/modules/admin/views/admin/invite-manager.php
@@ -5,20 +5,16 @@
 use backend\modules\authorization\models\RegistrationForm;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yiister\gentelella\widgets\Panel;
 
 $this->title = 'My Yii Application';
 ?>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-            <div class="x_title">
-                <h2>New manager registration form</h2>
-                <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                </ul>
-                <div class="clearfix"></div>
-            </div>
+        <?php Panel::begin([
+            'header' => Yii::t('app', 'Bids'),
+            'collapsable' => true,
+        ]) ?>
             <div class="x_content">
                 <br>
                     <?php $formRegistration = ActiveForm::begin(['action' => ['/invite-manager'],
@@ -91,73 +87,7 @@ $this->title = 'My Yii Application';
 
                 <?php ActiveForm::end(); ?>
             </div>
-        </div>
+        <?php Panel::end() ?>
     </div>
 </div>
-<!--<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
-<div class="row">
-    <div class="col s12 m10 offset-m1 l6 offset-l3 z-depth-5 forms-box">
 
-    <div id="registration" class="col s12">
-    <?php /*$formRegistration = ActiveForm::begin(['action' => ['/invite-manager'],
-        'class' => 'col s12 regForm', 'method' => 'post']); */?>
-    <div class="row">
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'role')->dropDownList([
-                'null'     => 'Выберите роль',
-                RegistrationForm::ROLE_ADMIN    => 'Админ',
-                RegistrationForm::ROLE_MANAGER  => 'Менеджер'
-            ])->label(false); */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'name',
-                ['template' => "{label}\n<i class=\"fa fa-user fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->textInput(['placeholder' => 'Имя пользователя'])
-                ->label(false); */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'last_name',
-                ['template' => "{label}\n<i class=\"fa fa-user fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->textInput(['placeholder' => 'Фамилия пользователя'])
-                ->label(false); */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'email',
-                ['template' => "{label}\n<i class=\"fa fa-envelope fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->textInput(['class' => 'validate', 'placeholder' => 'E-mail'])
-                ->label(false); */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'phone_number',
-                ['template' => "{label}\n<i class=\"fa fa-envelope fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->textInput(['class' => 'validate', 'placeholder' => 'Phone number'])
-                ->label(false); */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'password',
-                ['template' => "{label}\n<i class=\"fa fa-lock fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->passwordInput(['placeholder' => 'Пароль'])
-                ->label(false) */?>
-        </div>
-        <div class="input-field col s12">
-            <?/*= $formRegistration->field($modelRegistration, 'confirm_password',
-                ['template' => "{label}\n<i class=\"fa fa-lock fa-fw prefix\"
-                                    aria-hidden=\"true\"></i>\n{input}\n{hint}\n{error}"])
-                ->passwordInput(['placeholder' => 'Повторите пароль'])
-                ->label(false) */?>
-        </div>
-
-        <?/*= Html::submitButton('Зарегистрировать', [
-            'class' => 'btn waves-effect waves-light green reg-btn',
-            'name'  => 'registration-button'
-        ]); */?>
-    </div>
-    <?php /*ActiveForm::end(); */?>
-    </div>
-    </div>
-</div>-->
