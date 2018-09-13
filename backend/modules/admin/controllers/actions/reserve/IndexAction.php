548323564872bc0cac87f95b231c099b158b3a14
timur <timur.ablyalimov@ratkussoft.com>
2018-09-13 12:58:01 +0300

    Add the ability to change the sum of reserves in the table

--- backend/modules/admin/controllers/actions/reserve/IndexAction.php
+++ backend/modules/admin/controllers/actions/reserve/IndexAction.php
@@ -2,8 +2,8 @@
 
 namespace backend\modules\admin\controllers\actions\reserve;
 
-use common\models\reserve\ReserveEntitySearch;
-use yii\base\Action;
+use common\models\reserve\{ ReserveEntity as Reserve, ReserveEntitySearch };
+use yii\{ base\Action, helpers\Json };
 use Yii;
 
 class IndexAction extends Action
@@ -12,6 +12,13 @@ class IndexAction extends Action
     {
         $searchModel = new ReserveEntitySearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        if (Yii::$app->request->post('hasEditable')) {
+            $reserve = Reserve::findOne(Yii::$app->request->post('editableKey'));
+            $reserve->sum = current(Yii::$app->request->post('ReserveEntity'))['sum'] ?? 0;
+            $reserve->save(false, ['sum']);
+            return  Json::encode(['output' => round($reserve->sum, 2)]);
+        }
 
         return $this->controller->render('index', [
             'searchModel' => $searchModel,
