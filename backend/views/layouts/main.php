7f33d315a66cf252b705975e6a0ed7f5446141f5
timur <timur.ablyalimov@ratkussoft.com>
2018-09-14 13:49:37 +0300

    ~

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -75,7 +75,7 @@ $this->title = Yii::t('app', 'Dashboard');
                         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
                             <div class="menu_section">
-                                <h3><?= Yii::t('app', 'General') ?></h3>
+                                &nbsp;
                                 <?= Menu::widget([
                                     'items' => [
                                         [
