8ad22ad100e89bbe4ac9495ea350c298a96f6d3d
timur <timur.ablyalimov@ratkussoft.com>
2018-11-30 10:43:02 +0300

    Fix AccessUserStatusBehavior class

--- common/behaviors/AccessUserStatusBehavior.php
+++ common/behaviors/AccessUserStatusBehavior.php
@@ -1,31 +1,23 @@
 <?php
 namespace common\behaviors;
 
-use rest\modules\api\v1\authorization\models\RestUserEntity;
 use Yii;
+use rest\modules\api\v1\authorization\repository\AuthUserRepositoryInterface;
+use rest\modules\api\v1\authorization\entity\AuthUserEntity;
 use yii\{
     base\Behavior, web\HttpException
 };
 
-/**
- * Class AccessUserStatusBehavior
- *
- * @package common\behaviors
- */
 class AccessUserStatusBehavior extends Behavior
 {
-    /**
-     * @var array
-     */
     public $message;
 
-    /**
-     * @throws HttpException
-     */
     public function checkUserRole()
     {
-        $user = RestUserEntity::findOne(['id' => Yii::$app->user->identity->getId()]);
-        $statuses = [RestUserEntity::STATUS_UNVERIFIED, RestUserEntity::STATUS_BANNED, RestUserEntity::STATUS_DELETED];
+        /** @var AuthUserRepositoryInterface $userRepository */
+        $userRepository = Yii::$container->get(AuthUserRepositoryInterface::class);
+        $user = $userRepository->findOneById(Yii::$app->user->id);
+        $statuses = [AuthUserEntity::STATUS_UNVERIFIED, AuthUserEntity::STATUS_BANNED, AuthUserEntity::STATUS_DELETED];
 
         if (!$user || in_array($user->status, $statuses)) {
             throw new HttpException(403, $this->message);
